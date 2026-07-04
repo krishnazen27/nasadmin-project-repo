2c127c2a67bd71b55e6ec0d171220aca8a7d54ab
krishnazen27 <krishnazen27@gmail.com>
2026-07-04 15:43:38 +0100

    testing
    testing

--- ansible/roles/docker/files/webapp/app.py
+++ ansible/roles/docker/files/webapp/app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker on AWS EC2! new code added"
+    return "Hello from Docker on AWS EC2! new code added testing"
 
 app.run(host="0.0.0.0", port=80)
