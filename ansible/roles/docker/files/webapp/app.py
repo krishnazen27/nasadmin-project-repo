f3de25b1b2fc126b2317adcd566560f7730c3ca9
krishnazen27 <krishnazen27@gmail.com>
2026-07-04 19:14:18 +0100

    updated code with changes
    application version etc

--- ansible/roles/docker/files/webapp/app.py
+++ ansible/roles/docker/files/webapp/app.py
@@ -1,10 +1,21 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        version=os.getenv("APP_VERSION", "development")
+    )
+
+@app.route("/health")
+def health():
+    return {
+        "status": "UP",
+        "version": os.getenv("APP_VERSION", "development")
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
