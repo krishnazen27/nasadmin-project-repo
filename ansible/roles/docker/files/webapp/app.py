9f88d44b4137ac93ed5ae47a4ec88e29d3cf3a7a
krishnazen27 <krishnazen27@gmail.com>
2026-07-04 18:28:07 +0100

    updated EC2 wait and app code
    updated EC2 wait and app code

--- ansible/roles/docker/files/webapp/app.py
+++ ansible/roles/docker/files/webapp/app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Docker on AWS EC2! Demo app"
+    return "Hello from Docker on AWS EC2! Demo app code changes pushed"
 
 app.run(host="0.0.0.0", port=80)
